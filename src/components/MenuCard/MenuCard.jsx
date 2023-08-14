809323447d79d080fc3d3efbf4e4878ff5b59b16
Ak <akmoorthi002.com>
2023-08-14 20:25:33 +0530

    creating components

---
+++ src/components/MenuCard/MenuCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MenuCard = () => {
+  return (
+    <div>MenuCard</div>
+  )
+}
+
+export default MenuCard
---
+++ src/components/TableCard/TableCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TableCard = () => {
+  return (
+    <div>TableCard</div>
+  )
+}
+
+export default TableCard
